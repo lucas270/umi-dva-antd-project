0ea199294bb89a9d229e1d45103215adcfbcf4ef
lucas270 <754812492@qq.com>
2018-12-03 10:15:45 +0800

    创建dev分支，合并测试

--- src/pages/dataTransfer/index.jsx
+++ src/pages/dataTransfer/index.jsx
@@ -7,7 +7,7 @@ import {
 // 也可以写成jsx文件，与js文件一样。
 class BaseData extends Component {
   render() {
-    return <div>dds</div>;
+    return <div>创建dev分支，合并看看</div>;
   }
 }
 export default connect()(BaseData);
