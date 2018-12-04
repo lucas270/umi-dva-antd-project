d3542eee47732b1c55d5c2320aadcf0e55b79996
lucas270 <754812492@qq.com>
2018-12-04 15:53:05 +0800

    修改页面标题

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,66 +1,63 @@
 import React from 'react';
 import { Link } from 'dva/router';
-import {
-  Layout, Menu, Icon,
-} from 'antd';
+import DocumentTitle from 'react-document-title'; // 能动态修改页面title的
+import { Layout, Menu, Icon } from 'antd';
 import Header from './header';
 import styles from './index.css';
 
 const { Sider, Content } = Layout;
 
 function BasicLayout(props) {
+  const pageTitle = () => {
+    return '知识-Umi Pro';
+  };
+
   const { children } = props;
   return (
-    <Layout>
-      <Header />
-      <Layout style={{ marginTop: 64 }}>
-        <Sider
-          trigger={null}
-          collapsible
-        >
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.SubMenu
-              key="sub1"
-              title={(
-                <span>
-                  <Icon type="smile" />
-                  <span>数据传递</span>
-                </span>)}
-            >
-              <Menu.Item key="1">
-                <Link to="testPage">
-                  <Icon type="video-camera" />
-                  <span>models传递数据</span>
+    <DocumentTitle title={pageTitle()}>
+      <Layout>
+        <Header />
+        <Layout style={{ marginTop: 64 }}>
+          <Sider
+            trigger={null}
+            collapsible
+          >
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['testPage']}>
+              <Menu.SubMenu
+                key="sub1"
+                title={(
+                  <span>
+                    <Icon type="smile" />
+                    <span>数据传递</span>
+                  </span>)}
+              >
+                <Menu.Item key="testPage">
+                  <Link to="testPage">
+                    <Icon type="video-camera" />
+                    <span>models传递数据</span>
+                  </Link>
+                </Menu.Item>
+                <Menu.Item key="dataTransfer">
+                  <Link to="dataTransfer">
+                    <Icon type="user" />
+                    <span>学习数据流</span>
+                  </Link>
+                </Menu.Item>
+              </Menu.SubMenu>
+              <Menu.Item key="/">
+                <Link to="/">
+                  <Icon type="upload" />
+                  <span>首页</span>
                 </Link>
               </Menu.Item>
-              <Menu.Item key="2">
-                <Link to="dataTransfer">
-                  <Icon type="user" />
-                  <span>学习数据流</span>
-                </Link>
-              </Menu.Item>
-            </Menu.SubMenu>
-            <Menu.Item key="3">
-              <Link to="/">
-                <Icon type="upload" />
-                <span>首页</span>
-              </Link>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Content style={{
-          margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280,
-        }}
-        >
-          { children }
-        </Content>
+            </Menu>
+          </Sider>
+          <Content style={{ padding: 24, background: '#fff' }}>
+            { children }
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
-    // <div className={styles.normal}>
-    //   <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-    //   { children }
-    // </div>
-
+    </DocumentTitle>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button } from 'antd';
-import DocumentTitle from 'react-document-title';
 import router from 'umi/router';
 import styles from './index.css';
 
@@ -12,20 +11,16 @@ export default function () {
     router.push('/dataTransfer');
   };
   return (
-  // <React.Fragment>
-    <DocumentTitle title="知识·类型">
-      <div>
-        <div className={styles.welcome} />
-        <div className={styles.btns}>
-          <Button type="primary" onClick={testClick} style={{ marginRight: 10 }}>
+    <React.Fragment>
+      <div className={styles.welcome} />
+      <div className={styles.btns}>
+        <Button type="primary" onClick={testClick} style={{ marginRight: 10 }}>
             model传递数据页
-          </Button>
-          <Button type="primary" onClick={btnClick}>
+        </Button>
+        <Button type="primary" onClick={btnClick}>
             学习数据流
-          </Button>
-        </div>
+        </Button>
       </div>
-    </DocumentTitle>
-  // </React.Fragment>
+    </React.Fragment>
   );
 }
